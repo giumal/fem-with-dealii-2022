f20077b9a170791fd41311a7df0625d55d2cf123
Giulio Malenza <90334113+giumal@users.noreply.github.com>
2022-06-21 21:34:17 +0200

    Add figure step-3.0.png

--- labs/lab-03/include/step-3.h
+++ labs/lab-03/include/step-3.h
@@ -22,31 +22,14 @@
 #ifndef step3_include_file
 #define step3_include_file
 
-#include <deal.II/base/function.h>
-#include <deal.II/base/quadrature_lib.h>
-
-#include <deal.II/dofs/dof_handler.h>
-#include <deal.II/dofs/dof_tools.h>
 
 #include <deal.II/fe/fe_q.h>
-#include <deal.II/fe/fe_values.h>
-
-#include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/tria.h>
 
-#include <deal.II/lac/dynamic_sparsity_pattern.h>
-#include <deal.II/lac/full_matrix.h>
-#include <deal.II/lac/precondition.h>
-#include <deal.II/lac/solver_cg.h>
+#include <deal.II/dofs/dof_handler.h>
+
 #include <deal.II/lac/sparse_matrix.h>
 #include <deal.II/lac/vector.h>
-
-#include <deal.II/numerics/data_out.h>
-#include <deal.II/numerics/matrix_tools.h>
-#include <deal.II/numerics/vector_tools.h>
-
-#include <fstream>
-#include <iostream>
 
 // Forward declare the tester class
 class Step3Tester;
--- labs/lab-03/source/step-3.cc
+++ labs/lab-03/source/step-3.cc
@@ -18,6 +18,29 @@
  *          Luca Heltai, 2021
  */
 #include "step-3.h"
+#include <deal.II/base/function.h>
+#include <deal.II/base/quadrature_lib.h>
+
+#include <deal.II/dofs/dof_tools.h>
+
+#include <deal.II/fe/fe_values.h>
+
+#include <deal.II/grid/grid_generator.h>
+
+#include <deal.II/lac/dynamic_sparsity_pattern.h>
+#include <deal.II/lac/full_matrix.h>
+#include <deal.II/lac/precondition.h>
+#include <deal.II/lac/solver_cg.h>
+
+#include <deal.II/numerics/data_out.h>
+#include <deal.II/numerics/matrix_tools.h>
+#include <deal.II/numerics/vector_tools.h>
+
+
+
+#include <fstream>
+#include <iostream>
+
 
 using namespace dealii;
 
